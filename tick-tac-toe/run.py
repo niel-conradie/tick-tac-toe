5d1778cffa115f4c2b01ddaea5d8dd5cb234ed63
Niel Conradie <nielconradie93@gmail.com>
2022-06-02 11:34:15 +0200

    Update run.py
    Refactoring code.

--- tick-tac-toe/run.py
+++ tick-tac-toe/run.py
@@ -1,50 +1,17 @@
-import time
-
 from tic_tac_toe import TicTacToe
 from player import HumanPlayer, EasyComputerPlayer
 
 
-def start_game(game, x_player, o_player, print_game=True):
-    """ Start game with x_player and o_player. """
+def run():
+    """ Tic-Tac-Toe. """
+    run = TicTacToe()
 
-    if print_game:
-        print()
-        game.display_board_numbers()
+    player_x = HumanPlayer('X')
+    player_o = EasyComputerPlayer('O')
 
-    # Starting letter.
-    letter = 'X'
-
-    while game.empty_squares():
-        # Selecting appropriate player.
-        if letter == 'O':
-            square = o_player.move(game)
-        else:
-            square = x_player.move(game)
-
-        # Function to make a move.
-        if game.valid_move(square, letter):
-            if print_game:
-                print(f"\n{letter} makes a move to square {square}\n")
-                game.display_board_numbers()
-                print("-------------")
-                game.display_board()
-
-            if game.current_winner:
-                if print_game:
-                    print(f"\n{letter} Wins!")
-                return letter
-
-            # Switching players.
-            letter = 'O' if letter == 'X' else 'X'
-
-        time.sleep(0.5)
-
-    if print_game:
-        print("\nTie!")
+    # Starting the game.
+    run.start_game(run, player_x, player_o, print_game=True)
 
 
 if __name__ == '__main__':
-    x_player = HumanPlayer('X')
-    o_player = EasyComputerPlayer('O')
-    start = TicTacToe()
-    start_game(start, x_player, o_player, print_game=True)
+    run()
