5baed872e2039ed8661a207b9b8d6797453aeecd
Niel Conradie <nielconradie93@gmail.com>
2022-06-16 09:35:25 +0200

    Update run.py
    Added restart method to run.py file.

--- tick-tac-toe/run.py
+++ tick-tac-toe/run.py
@@ -9,8 +9,14 @@ def run():
     player_x = HumanPlayer('X')
     player_o = EasyComputerPlayer('O')
 
-    # Starting the game.
-    run.start_game(run, player_x, player_o, print_game=True)
+    while True:
+        start = TicTacToe()
+        # Starting the game.
+        run.start_game(start, player_x, player_o, print_game=True)
+        # Requesting user input.
+        run.restart()
+
+        continue
 
 
 if __name__ == '__main__':
