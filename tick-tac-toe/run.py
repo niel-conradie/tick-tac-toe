eb393d3f2aa542b7ce55bf6adc9186170af69272
Niel Conradie <nielconradie93@gmail.com>
2022-07-28 11:16:09 +0200

    Update run.py
    Removing old unused code, and added start_game() method.

--- tick-tac-toe/run.py
+++ tick-tac-toe/run.py
@@ -3,32 +3,10 @@ from tic_tac_toe import TicTacToe
 
 def run():
     """Tic-Tac-Toe."""
-    game = TicTacToe()
+    run = TicTacToe()
 
-    x = "X"
-    # Display player X options.
-    game.display_options(x)
-    # Requesting user input.
-    player_x_input = game.player_x_input()
-    # Assign player X input to appropriate type of player.
-    player_x = game.user_input_allocation(x, player_x_input)
-
-    o = "O"
-    # Display player O options.
-    game.display_options(o)
-    # Requesting user input.
-    player_o_input = game.player_o_input()
-    # Assign player O input to appropriate type of player.
-    player_o = game.user_input_allocation(o, player_o_input)
-
-    while True:
-        start = TicTacToe()
-        # Starting game and passing player inputs as arguments.
-        game.start_game(start, player_x, player_o, print_game=True)
-        # Requesting user input.
-        game.restart()
-
-        continue
+    # Starting the game.
+    run.start_game()
 
 
 if __name__ == "__main__":
